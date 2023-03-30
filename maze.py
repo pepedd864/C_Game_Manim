b91376702e7d22b9bb4253887edf79af9bb1739a
pepedd864 <pepedd@qq.com>
2023-03-30 10:23:40 +0800

    modified:   .gitignore 	new file:   README.md 	modified:   maze.py

--- maze.py
+++ maze.py
@@ -1,33 +1,11 @@
 from manimlib import *
 
-
-def create_block(type):
-    square = Square(0.3)
-    square.set_fill(BLUE if type == 0 else BLACK, opacity=0.7)
-    square.set_color(BLUE if type == 0 else BLACK)
-    # label = Text(str(type), font_size=18, font="思源黑体")
-    # square.add(label)
-    return square
-
-
-def create_grid(grid):
-    group = VGroup()
-    for i in grid:
-        inner = VGroup()
-        for j in i:
-            inner.add(create_block(j))
-        inner.arrange(RIGHT, buff=0)
-        group.add(inner)
-    group.arrange(DOWN, buff=0)
-    return group
-
-
 width = 31
 height = 21
 map = [[0 for _ in range(width)] for _ in range(height)]  # 0表示未访问过，1表示访问过
 
 
-def dig(self, x, y):
+def dig(x, y):
     # 使用2的原因是利用间隔来形成墙
     a = [[2, 0], [-2, 0], [0, 2], [0, -2]]  # 方向 右 左 下 上
     for i in range(4):
@@ -50,9 +28,38 @@ def dig(self, x, y):
         dig(x + a[i][0], y + a[i][1])
 
 
+def create_block(type):
+    square = Square(0.3)
+    square.set_fill(BLUE if type == 0 else BLACK, opacity=0.7)
+    square.set_color(BLUE if type == 0 else BLACK)
+    # label = Text(str(type), font_size=18, font="思源黑体")
+    # square.add(label)
+    return square
+
+
+def create_slider(x, y, z=0):
+    slider = Square(0.3, fill_opacity=0.7, fill_color=RED, stroke_width=0)
+    slider.move_to([x, y, z])
+    return slider
+
+
+def create_grid(grid):
+    group = VGroup()
+    for i in grid:
+        inner = VGroup()
+        for j in i:
+            inner.add(create_block(j))
+        inner.arrange(RIGHT, buff=0)
+        group.add(inner)
+    group.arrange(DOWN, buff=0)
+    return group
+
+
 class Maze(Scene):
     def construct(self):
         group = create_grid(map)
         self.play(Write(group))
-        self.scene.remove(self.scene.mobjects[-1])
-        # dig(self, 1, 1)
+        dig(1, 1)
+        self.play(Transform(group, create_grid(map)))
+        self.wait(1)
+        self.add(create_slider(group[1][1].get_x(), group[1][1].get_y()))
